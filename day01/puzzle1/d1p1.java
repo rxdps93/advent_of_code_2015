97515c0fdb698ef89a083f1f9e76fcdf267d5216
rxdps93 <dpsirott@gmail.com>
2022-12-01 17:07:00 -0500

    initial commit

---
+++ day01/puzzle1/d1p1.java
@@ -0,0 +1,7 @@
+package puzzle1;
+
+public class d1p1 {
+    public static void main(String[] args) {
+        System.out.println("d1p1");
+    }
+}
---
+++ day01/puzzle2/d1p2.java
@@ -0,0 +1,7 @@
+package puzzle2;
+
+public class d1p2 {
+    public static void main(String[] args) {
+        System.out.println("d1p2");
+    }
+}
