f56b21755dcbad8e190860f3d8b324d930b946f1
rxdps93 <dpsirott@gmail.com>
2022-12-02 11:03:34 -0500

    day 1 puzzle 2 complete

--- day01/puzzle2/d1p2.java
+++ day01/puzzle2/d1p2.java
@@ -1,7 +1,35 @@
 package puzzle2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class d1p2 {
     public static void main(String[] args) {
-        System.out.println("d1p2");
+
+        try {
+            FileInputStream input = new FileInputStream("day01/input.txt");
+
+            int c;
+            int floor = 0, pos = 0, bsmnt = 0;
+            while ((c = input.read()) != -1) {
+                if ((char)c == '(') {
+                    floor++;
+                    pos++;
+                } else if ((char)c == ')') {
+                    floor--;
+                    pos++;
+                }
+
+                if (floor < 0 && bsmnt == 0) {
+                    bsmnt = pos;
+                }
+            }
+
+            System.out.println("Ends On Floor: " + floor);
+            System.out.println("Entered Basement: " + bsmnt);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
