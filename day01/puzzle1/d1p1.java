82e2a2ddf72b3c4696e7a922ad6ae390c9e673f9
rxdps93 <dpsirott@gmail.com>
2022-12-02 10:59:23 -0500

    day 1 puzzle 1 complete

--- day01/puzzle1/d1p1.java
+++ day01/puzzle1/d1p1.java
@@ -1,7 +1,28 @@
 package puzzle1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class d1p1 {
     public static void main(String[] args) {
-        System.out.println("d1p1");
+
+        try {
+            FileInputStream input = new FileInputStream("day01/input.txt");
+
+            int c;
+            int floor = 0;
+            while ((c = input.read()) != -1) {
+                if ((char)c == '(') {
+                    floor++;
+                } else if ((char)c == ')') {
+                    floor--;
+                }
+            }
+
+            System.out.println("Floor: " + floor);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
