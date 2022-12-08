4d60ea4410c16249fd6cf83bb9e729f7bd40e76b
rxdps93 <dpsirott@gmail.com>
2022-12-08 11:40:20 -0500

    day 5 puzzle 1 complete

--- day05/puzzle1/d5p1.java
+++ day05/puzzle1/d5p1.java
@@ -7,37 +7,13 @@ import java.util.regex.Pattern;
 
 public class d5p1 {
 
-    private static void parseVowels(List<String> lines) {
+    private static void parseVowels(List<String>lines) {
 
-        final String vowels = "aeiou";
-
-        for (int i = 0; i < lines.size(); i++) {
-            int vc = 0;
-            for (int c = 0; c < lines.get(i).length(); c++) {
-                if (vowels.contains(Character.toString(lines.get(i).charAt(c)))) {
-                    vc++;
-                }
-
-                if (vc == 3) {
-                    break;
-                }
-            }
-
-            if (vc < 3) {
-                System.out.println("removing " + lines.get(i));
-                lines.remove(i);
-            }
-        }
-    }
-
-    private static void parseVowelsRegex(List<String>lines) {
-
-        final String regex = "[aeiou]+";
+        final String regex = ".*[aeiou].*[aeiou].*[aeiou].*";
 
         for (int i = 0; i < lines.size(); i++) {
             if (!Pattern.matches(regex, lines.get(i))) {
-                System.out.println("removing " + lines.get(i));
-                lines.remove(i);
+                lines.set(i, "");
             }
         }
     }
@@ -48,25 +24,29 @@ public class d5p1 {
 
         for (int i = 0; i < lines.size(); i++) {
             if (!Pattern.matches(regex, lines.get(i))) {
-                System.out.println("removing " + lines.get(i));
-                lines.remove(i);
+                lines.set(i, "");
             }
         }
     }
 
     private static void parseForbidden(List<String> lines) {
-        final String[] forbidden = { "ab", "cd", "pq", "xy" };
+
+        final String regex = ".*(ab|cd|pq|xy).*";
+
+        for (int i = 0; i < lines.size(); i++) {
+            if (Pattern.matches(regex, lines.get(i))) {
+                lines.set(i, "");
+            }
+        }
     }
 
     public static void main(String[] args) {
-        List<String> lines = new InputParser("day05/testInput.txt").readLinesAsList();
+        List<String> lines = new InputParser("day05/input.txt").readLinesAsList();
 
-//        System.out.println("vowel check");
-//        parseVowels(lines);
-        System.out.println("now with regex");
-        parseVowelsRegex(lines);
-//        System.out.println("double check");
-//        parseDoubles(lines);
+        parseVowels(lines);
+        parseDoubles(lines);
+        parseForbidden(lines);
+        lines.removeIf(""::equals);
 
         System.out.println(lines.size());
     }
