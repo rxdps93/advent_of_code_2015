a27813c1dd67e32c9fd49d059a166c23f12d0ac1
rxdps93 <dpsirott@gmail.com>
2022-12-09 18:05:22 -0500

    prep

---
+++ day07/puzzle1/d7p1.java
@@ -0,0 +1,13 @@
+package day07.puzzle1;
+
+import utils.InputParser;
+
+import java.util.List;
+
+public class d7p1 {
+
+    public static void main(String[] args) {
+        List<String> lines = new InputParser("day07/input.txt").readLinesAsList();
+
+    }
+}
