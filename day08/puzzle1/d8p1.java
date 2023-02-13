a21c756c28f66c74b1540473c15b5dd915384c52
rxdps93 <dpsirott@gmail.com>
2023-02-13 09:56:54 -0500

    day 8 puzzle 1 complete

--- day08/puzzle1/d8p1.java
+++ day08/puzzle1/d8p1.java
@@ -9,7 +9,21 @@ import java.util.regex.Matcher;
 
 public class d8p1 {
 
+    private static String convert(String old) {
+        String str = old;
 
+        Pattern hexPattern = Pattern.compile("\\\\x[0-9|A-F|a-f]{2}");
+
+        Matcher hm = hexPattern.matcher(str);
+        while (hm.find()) {
+            String hex = hm.group().substring(2);
+            str = str.replace(hm.group(0), Character.toString((char)Integer.parseInt(hex, 16)));
+        }
+
+        str = str.replace("\\\"", "\"").replace("\\\\", "\\");
+
+        return str;
+    }
 
     public static void main(String [] args) {
         List<String> lines = new InputParser("day08/input.txt").readLinesAsList();
@@ -17,23 +31,10 @@ public class d8p1 {
         int strSum = 0;
         int memSum = 0;
 
-        Pattern pattern = Pattern.compile("\\\\x[0-9|A-F]{2}");
         for (String line : lines) {
             strSum += line.length();
-            String str = line.substring(1, line.lastIndexOf("\""))
-                    .replace("\\\"", "\"")
-                    .replace("\\\\", "\\");
-
-            Matcher matcher = pattern.matcher(str);
-            while (matcher.find()) {
-                String hex = matcher.group().substring(2);
-                str = str.replace(matcher.group(0), Character.toString((char)Integer.parseInt(hex, 16)));
-            }
-
+            String str = convert(line.substring(1, line.lastIndexOf("\"")));
             memSum += str.length();
-
-            System.out.printf("%10s\t%d\n", line, line.length());
-            System.out.printf("%10s\t%d\n\n", str, str.length());
         }
 
         System.out.println("The answer is " + (strSum - memSum));
