262f6495e5012c79cbe5d27c1c8ed44489e7f8c6
rxdps93 <dpsirott@gmail.com>
2023-02-15 09:49:13 -0500

    day 14 puzzle 1 complete

--- day14/puzzle1/d14p1.java
+++ day14/puzzle1/d14p1.java
@@ -2,42 +2,41 @@ package day14.puzzle1;
 
 import utils.InputParser;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class d14p1 {
 
-    private static class Reindeer {
-
-        public final String name;
-        public final int speed;
-        public final int duration;
-        public final int rest;
-        public int canMoveOn = 0;
-
-        public Reindeer(String name, int speed, int duration, int rest) {
-            this.name = name;
-            this.speed = speed;
-            this.duration = duration;
-            this.rest = rest;
-        }
-    }
-
     public static void main(String[] args) {
 
-        List<String> input = InputParser.parse("day14/test.txt").asList();
+        List<String> input = InputParser.parse("day14/input.txt").asList();
 
-        List<Reindeer> reindeers = new ArrayList<>();
-        input.forEach(line -> {
+        String bestName = "";
+        int bestDistance = Integer.MIN_VALUE;
+        final double seconds = 2503;
+        for (String line : input) {
             String[] tokens = line.split(" ");
-            reindeers.add(new Reindeer(
-                    tokens[0],
-                    Integer.parseInt(tokens[3]),
-                    Integer.parseInt(tokens[6]),
-                    Integer.parseInt(tokens[tokens.length - 2])));
-        });
+            int speed = Integer.parseInt(tokens[3]);
+            double moveDuration = Double.parseDouble(tokens[6]);
+            double restDuration = Double.parseDouble(tokens[tokens.length - 2]);
 
+            double fullCycles = Math.floor(seconds / (moveDuration + restDuration));
+            double remainder = seconds % (moveDuration + restDuration);
 
+            int dist = 0;
+            if (remainder >= moveDuration) {
+                dist += speed * moveDuration;
+            } else {
+                dist += speed * remainder;
+            }
+
+            dist += fullCycles * (speed * moveDuration);
+
+            if (dist > bestDistance) {
+                bestName = tokens[0];
+                bestDistance = dist;
+            }
+        }
+
+        System.out.printf("The winning reindeer, %s, has travelled %d km after exactly %d seconds.", bestName, bestDistance, (int)seconds);
     }
 }
